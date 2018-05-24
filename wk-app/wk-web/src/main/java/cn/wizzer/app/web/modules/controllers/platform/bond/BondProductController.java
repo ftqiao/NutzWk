2c5fced682f1fb5dbf0a4794aba8651d8556c1a0
ftqiao <ftqiao@163.com>
2018-05-24 15:53:40 +0800

    实现增删改demo

--- wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/platform/bond/BondProductController.java
+++ wk-app/wk-web/src/main/java/cn/wizzer/app/web/modules/controllers/platform/bond/BondProductController.java
@@ -6,11 +6,14 @@ import cn.wizzer.app.cms.modules.models.Cms_article;
 import cn.wizzer.app.cms.modules.models.Cms_channel;
 import cn.wizzer.app.cms.modules.services.CmsArticleService;
 import cn.wizzer.app.cms.modules.services.CmsChannelService;
+import cn.wizzer.app.sys.modules.models.Sys_config;
 import cn.wizzer.app.sys.modules.models.Sys_user;
+import cn.wizzer.app.web.commons.base.Globals;
 import cn.wizzer.app.web.commons.slog.annotation.SLog;
 import cn.wizzer.framework.base.Result;
 import cn.wizzer.framework.page.datatable.DataTableColumn;
 import cn.wizzer.framework.page.datatable.DataTableOrder;
+import cn.wizzer.framework.rabbit.RabbitMessage;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.subject.Subject;
@@ -18,13 +21,11 @@ import org.nutz.dao.Cnd;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.lang.Strings;
+import org.nutz.lang.util.NutMap;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.nutz.mvc.adaptor.WhaleAdaptor;
-import org.nutz.mvc.annotation.AdaptBy;
-import org.nutz.mvc.annotation.At;
-import org.nutz.mvc.annotation.Ok;
-import org.nutz.mvc.annotation.Param;
+import org.nutz.mvc.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
@@ -51,39 +52,63 @@ public class BondProductController {
 
     @At
     @Ok("beetl:/platform/bond/product/add.html")
-    //@RequiresPermissions("cms.content.article")
-    public void add(@Param("channelId") String channelId, HttpServletRequest req) {
-//        Subject subject = SecurityUtils.getSubject();
-//        Sys_user user = (Sys_user) subject.getPrincipal();
-//        req.setAttribute("username", user == null ? "" : user.getUsername());
-        log.info("增加一个产品成功");
+    public void add() {
     }
+
+    @At
+    @Ok("json")
+    //@RequiresPermissions("cms.content.article")
+    public Object addDo(@Param("..") Bond_product bond_product) {
+        try {
+            if (bondProductService.insert(bond_product) != null) {
+                return Result.success("system.success");
+            }
+            return Result.error("system.error");
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
 
     @At("/edit/?")
     @Ok("beetl:/platform/bond/product/edit.html")
+    public Object edit(String id) {
+        return bondProductService.fetch(id);
+    }
+
+    @At
+    @Ok("json")
     //@RequiresPermissions("cms.content.article")
-    public Object edit(String id, HttpServletRequest req) {
-        Bond_product product = bondProductService.fetch(id);
-        req.setAttribute("product", product);
-        return product;
+    public Object editDo(@Param("..") Bond_product bond_product) {
+        if (bondProductService.updateIgnoreNull(bond_product) > 0) {
+            return Result.success("system.success");
+        }
+        return Result.error("system.error");
     }
 
     @At({"/delete/?", "/delete"})
     @Ok("json")
-    @RequiresPermissions("cms.content.article.delete")
-    @SLog(tag = "删除文章", msg = "ID:${args[2].getAttribute('id')}")
-    public Object delete(String oneId, @Param("ids") String[] ids, HttpServletRequest req) {
+    public Object delete(String id, @Param("ids") String[] ids, HttpServletRequest req) {
         try {
             if (ids != null && ids.length > 0) {
                 bondProductService.delete(ids);
                 req.setAttribute("id", org.apache.shiro.util.StringUtils.toString(ids));
             } else {
-                bondProductService.delete(oneId);
-                req.setAttribute("id", oneId);
+                bondProductService.delete(id);
+                req.setAttribute("id", id);
             }
             return Result.success("system.success");
         } catch (Exception e) {
             return Result.error("system.error");
         }
+    }
+
+    @At
+    @Ok("json:full")
+    public Object data(@Param("unitid") String unitid, @Param("length") int length, @Param("start") int start, @Param("draw") int draw, @Param("::order") List<DataTableOrder> order, @Param("::columns") List<DataTableColumn> columns) {
+        Cnd cnd = Cnd.NEW();
+        if (!Strings.isBlank(unitid) && !"root".equals(unitid))
+            cnd.and("unitid", "=", unitid);
+        return bondProductService.data(length, start, draw, order, columns, cnd, null);
     }
 }
